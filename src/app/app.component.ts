ee79c5c8f392c0a7d60be0593ec9e1538ce8f58d
Anderson Olliver <pla.olliver@gmail.com>
2020-08-14 19:59:59 -0400

    adiciona exemplo de formularios do estilo template-driven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Login } from './model/login';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'rf-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'projeto-teste';
+  public login: Login;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.login = new Login();
+    // this.login.email = 'teste';
+    this.login.senha = 'teste';
+  }
+
+  entrar() {
+    console.log(this.login);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TesteComponent
-  ],
-  imports: [
-    BrowserModule,
-  ],
+  declarations: [AppComponent, TesteComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
