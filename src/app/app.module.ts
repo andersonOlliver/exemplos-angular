1176801d51b0de8ece42b2b248b2dd11b0d2feab
Anderson Olliver <pla.olliver@gmail.com>
2020-08-20 18:06:02 -0400

    adiciona integração com formulario reativo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TesteComponent } from './teste/teste.component';
 import { LoginPageComponent } from './pages/public/login-page/login-page.component';
 import { CriaUsuarioPageComponent } from './pages/public/cria-usuario-page/cria-usuario-page.component';
-import { Routes } from '@angular/router';
-import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +15,7 @@ import { RouterModule } from '@angular/router';
     LoginPageComponent,
     CriaUsuarioPageComponent,
   ],
-  imports: [AppRoutingModule, BrowserModule, FormsModule],
+  imports: [AppRoutingModule, BrowserModule, FormsModule, ReactiveFormsModule ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/public/cria-usuario-page/cria-usuario-page.component.ts
+++ src/app/pages/public/cria-usuario-page/cria-usuario-page.component.ts
@@ -1,14 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   templateUrl: './cria-usuario-page.component.html',
-  styleUrls: ['./cria-usuario-page.component.scss']
+  styleUrls: ['./cria-usuario-page.component.scss'],
 })
 export class CriaUsuarioPageComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.construirFormulario();
+
+    // const emailCtrl = this.fb.control('x');
   }
 
+  construirFormulario(usuario?: any) {
+    this.form = this.fb.group({
+      email: [
+        usuario?.email || '',
+        Validators.compose([
+          Validators.required,
+          Validators.email,
+          Validators.minLength(10),
+          Validators.maxLength(100),
+        ]),
+      ],
+      password: [
+        usuario?.password || '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(32),
+        ]),
+      ],
+      confirmPassword: [
+        usuario?.confirmPassword || '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(32),
+        ]),
+      ],
+    });
+  }
+
+  criar() {
+    console.log(this.form);
+    console.log(this.form.value);
+  }
+
+  estaInvalido(controle: string) {
+    if (controle) {
+      return this.form.get(controle).touched && this.form.get(controle).invalid;
+    }
+  }
 }
